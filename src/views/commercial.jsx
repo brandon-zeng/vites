793c6ac41387ea6ecfc234b245517969d8a807d5
Wenjuan Zhang <wenjuanzhang@Wenjuan-Zhangs-MacBook-Pro.local>
2017-01-28 01:50:27 -0500

    added some animation

---
+++ src/views/commercial.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ComponentStyle from '../styles/content.less';
+
+class ContactsComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	getIdFromVideoString (vString) {
+		const urlArr = vString.split('/');
+		const idString = urlArr[urlArr.length - 1];
+		const queryParams = qs.extract(vString);
+
+		return (queryParams && qs.parse(queryParams).v) || idString || '';
+	}
+
+	render() {
+		{/*
+		const {service, video, ...htmlTags} = this.props;
+		const src = `${SocialVideo.urlMap.get(service)}${this.getIdFromVideoString(video)}`;
+		*/}
+
+		return (
+			<div style={{paddingTop: 200, flex: 1}}>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/rAcaTxSMIho" frameborder="0" 
+					allowfullscreen 
+					webkitAllowFullScreen 
+					mozallowfullscreen
+					style={{margin: '0 auto', display: 'block'}}></iframe>
+			</div>
+		);
+	}
+}
+
+export default ContactsComponent;
